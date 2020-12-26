f1e40c313be6f5646ba6c2cd80acab9987855be5
unknown <wahid.warid55@gmail.com>
2020-12-26 07:04:01 +0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.set("port", process.env.PORT || 8080)
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(app.get("port"), () => {
+	console.log("Application running on port " + app.get('port'))
+})
