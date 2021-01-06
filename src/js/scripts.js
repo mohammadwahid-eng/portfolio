ea7f624237859c298ae20b1e728aa548ddb2e9b1
unknown <wahid.warid55@gmail.com>
2021-01-06 07:49:39 +0600

    particle.js, typed.js added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,27 @@
+require("jquery")
 require("bootstrap")
+require("./particles")
 require("../scss/style.scss")
+import Typed from "typed.js";
+
+
+
+
+
+
+var typed = new Typed('.typed-content', {
+	strings: [
+		"Mohammad Wahid",
+		"a Freelancer",
+		"a Web Developer"
+	],
+	typeSpeed: 80,
+	backDelay: 1000,
+	loop: true
+});
+
+
+
+
+
+
